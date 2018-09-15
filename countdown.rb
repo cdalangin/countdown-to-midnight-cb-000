c09bb202a427b49974ece526cede16b5187ccd02
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-09-15 22:48:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(num)
-  num = num.to_i
-  while num > 1
-    num -= 1
+def countdown(number)
+  number = number.to_i
+  while number > 1
+    number -= 1
     puts "#{integer} SECOND(S)!"
     sleep(1)
   end
